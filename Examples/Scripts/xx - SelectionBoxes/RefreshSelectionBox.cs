bb88239602d38851ab6fedc2d519ba0b29624cb9
Invex Games <invexgames@gmail.com>
2015-03-26 23:19:31 +1000

    - Added a way to re-populate the list items in a SelectionBox (see the SelectionBoxes example)

---
+++ Examples/Scripts/xx - SelectionBoxes/RefreshSelectionBox.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace MaterialUI
+{
+	public class RefreshSelectionBox : MonoBehaviour
+	{
+		[SerializeField] private SelectionBoxConfig box;
+
+		public void Refresh ()
+		{
+			box.listItems = new string[6];
+
+			box.listItems[0] = "D";
+			box.listItems[1] = "E";
+			box.listItems[2] = "F";
+			box.listItems[3] = "G";
+			box.listItems[4] = "H";
+			box.listItems[5] = "I";
+
+			box.RefreshList();
+		}
+	}
+}
